d73036d899dc32c1ab8a7b6e26cb3851a5301a6a
razin92 <s.razin92@gmail.com>
2017-10-03 17:22:07 +0500

    Откат

--- salary/forms.py
+++ salary/forms.py
@@ -7,7 +7,7 @@ class BonusWorkForm(forms.Form):
     date = forms.DateField(label="Дата", initial=datetime.date.today(), widget=DateTimePicker(options={"format": "YYYY-MM-DD"}))
     model = forms.ModelChoiceField(label="Работа", queryset=WorkCalc.objects.all().order_by('name'))
     worker = forms.ModelChoiceField(label="Работник", queryset=Worker.objects.all().order_by('name__firstname'))
-    quantity = forms.IntegerField(label="Кол-во", min_value=0.5, max_value=100, initial=1)
+    quantity = forms.IntegerField(label="Кол-во", min_value=1, max_value=100, initial=1)
     comment = forms.CharField(label="Комментарий", max_length=100, required=False)
     withholding = forms.BooleanField(label="Удержание", required=False)
 
