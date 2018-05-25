e64ecc1c09a924b1142577b255607ec36825bc46
razin92 <s.razin92@gmail.com>
2018-05-25 13:16:34 +0500

    WorkReports Admin side edited

--- salary/admin.py
+++ salary/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import WorkCalc, BonusWork, CategoryOfChange, Worker, AccountChange, Total, Work, WorkReport
 
 # Register your models here.
+class WorkReportAdmin(admin.ModelAdmin):
+    list_display = ['working_date', 'user', 'quarter', 'apartment',
+                    'building', 'work', 'cost', 'confirmed', 'deleted']
+
+    date_hierarchy = 'working_date'
+
 admin.site.register(WorkCalc)
 admin.site.register(BonusWork)
 admin.site.register(CategoryOfChange)
@@ -9,4 +15,4 @@ admin.site.register(Worker)
 admin.site.register(AccountChange)
 admin.site.register(Total)
 admin.site.register(Work)
-admin.site.register(WorkReport)
+admin.site.register(WorkReport, WorkReportAdmin)
