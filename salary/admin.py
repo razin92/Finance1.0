e05e7a9b6158090435f1ccf6a8e99eb904c301f4
razin92 <s.razin92@gmail.com>
2018-08-01 10:41:12 +0500

    add column filter in admin for total model

--- salary/admin.py
+++ salary/admin.py
@@ -9,11 +9,14 @@ class WorkReportAdmin(admin.ModelAdmin):
     date_hierarchy = 'working_date'
     search_fields = ['id']
 
+class TotalAdmin(admin.ModelAdmin):
+    list_display = ['date', 'worker', 'balance_before', 'balance_now']
+
 admin.site.register(WorkCalc)
 admin.site.register(BonusWork)
 admin.site.register(CategoryOfChange)
 admin.site.register(Worker)
 admin.site.register(AccountChange)
-admin.site.register(Total)
+admin.site.register(Total, TotalAdmin)
 admin.site.register(Work)
 admin.site.register(WorkReport, WorkReportAdmin)
